1c1eec36c63cc8becdc15a65c15463aee7c25010
drili <DriliB@hotmail.com>
2023-01-15 00:28:37 +0100

    :construction: WIP - task view

--- AJAX/globals/AJAX_fetch_task.php
+++ AJAX/globals/AJAX_fetch_task.php
@@ -22,12 +22,21 @@
 ?>
 
 <?php if ($response["query_status"] !== "ERR_FETCHING_TASKS_BY_USER") : ?>
-    <div>
-        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Magni eos fugiat numquam ex harum a repellat, rerum non aliquid consequatur mollitia perspiciatis id corrupti suscipit sunt dicta quae voluptatibus est obcaecati iure ea libero? Deleniti doloribus quas aliquam exercitationem est eum harum eaque, a eveniet neque impedit nemo eligendi nobis?</p>
-
-        <h2>Lorem ipsum dolor sit, amet consectetur adipisicing elit. A, reprehenderit!</h2>
-
-        <?php var_dump($response); ?>
+    <div>    
+        <p>task_id: <?php echo $response["query_status"][0]["task_id"]; ?></p>
+        <p>task_name: <?php echo $response["query_status"][0]["task_name"]; ?></p>
+        <p>task_low: <?php echo $response["query_status"][0]["task_low"]; ?></p>
+        <p>task_high: <?php echo $response["query_status"][0]["task_high"]; ?></p>
+        <p>task_workflow_status: <?php echo $response["query_status"][0]["task_workflow_status"]; ?></p>
+        <p>task_description: <?php echo $response["query_status"][0]["task_description"]; ?></p>
+        <p>sprint_id: <?php echo $response["query_status"][0]["sprint_id"]; ?></p>
+        <p>customer_id: <?php echo $response["query_status"][0]["customer_id"]; ?></p>
+        <p>label_id: <?php echo $response["query_status"][0]["label_id"]; ?></p>
+        <p>task_type_id: <?php echo $response["query_status"][0]["task_type_id"]; ?></p>
+        <p>task_vertical_id: <?php echo $response["query_status"][0]["task_vertical_id"]; ?></p>
+        <p>is_external: <?php echo $response["query_status"][0]["is_external"]; ?></p>
+        <p>created_at: <?php echo $response["query_status"][0]["created_at"]; ?></p>
+        <p>created_by: <?php echo $response["query_status"][0]["created_by"]; ?></p>
     </div>
 
 <?php else : ?>
